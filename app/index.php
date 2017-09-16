34c429f5746f27918a4af1864441ac1098e5be2a
kong lin <wuyou7410@gmail.com>
2017-09-16 23:26:39 +0800

    暂存

--- app/index.php
+++ app/index.php
@@ -5,4 +5,4 @@ require_once(dirname(__DIR__) . '/vendor/autoload.php');
 \app\Bootstrap::bootstrap(
     'app',
     new \Tiny\Application(require(dirname(__DIR__) . "/config/app-config.ignore.php"))
-)->run();
+)->run( new \Tiny\Request($_ENV, $_SERVER, $_GET, $_POST, $_REQUEST, $_COOKIE, $_SESSION, $_FILES), new \Tiny\Response());
